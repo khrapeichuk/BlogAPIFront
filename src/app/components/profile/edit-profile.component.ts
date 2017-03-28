d62dc0ba5c1492d59578fd8ae0090a92b5740293
vhrapeiciuc <vitalina.hrapeichuk@gmail.com>
2017-03-28 16:38:06 +0300

    add validator to edit profile form

--- src/app/components/profile/edit-profile.component.ts
+++ src/app/components/profile/edit-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { UserService } from '../../user/user.service';
 
@@ -15,8 +16,17 @@ import {LocalStorageService} from "../../local-storage.service";
 export class EditProfileComponent implements OnInit {
   data: Object;
   error: null;
+  editProfileForm : FormGroup;
 
-  constructor(private userService: UserService, private  localStorageService: LocalStorageService, private router: Router) { }
+  constructor(fb: FormBuilder, private userService: UserService, private  localStorageService: LocalStorageService, private router: Router) {
+    let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+
+    this.editProfileForm = fb.group({
+      'firstname': ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(15)])],
+      'lastname': ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(15)])],
+      'email': ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])]
+    })
+  }
 
   ngOnInit(): void {
     this.getUserData();
